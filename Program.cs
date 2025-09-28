ce95c38df02aeeb2b0f23dce06e7be0c980516dc
Diogo Nascimento <diogocoding@gmail.com>
2025-09-27 22:17:49 -0300

    projeto finalizado com sucessooogit add .!

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
+
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Nokia n1 = new Nokia(numero: "81818181", modelo: "4545", imei: "5151", memoria: 454);
+n1.Ligar();
+n1.ReceberLigacao();
+n1.InstalarAplicativo(nomeApp: "gitHub");
+
+Console.WriteLine("\n");
+
+Iphone I1 = new Iphone(numero: "81818181", modelo: "4545", imei: "5151", memoria: 454);
+I1.Ligar();
+I1.ReceberLigacao();
+I1.InstalarAplicativo(nomeApp: "Linkedin");
